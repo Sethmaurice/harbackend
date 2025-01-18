bb3bac35dd60b94b2bc29b1691234b9b08460243
Sethmaurice <sethmaurice1@gmail.com>
2025-01-18 13:08:31 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   // Enable CORS for frontend (React app running on localhost:3000)
   app.enableCors({
     origin: 'http://localhost:3000', // Frontend URL
+    // origin: https://harambeerw.vercel.app/
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
     credentials: true, // If you're using cookies or JWT
@@ -16,7 +17,9 @@ async function bootstrap() {
     // Use global validation pipes for input validation
     // app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 4000); // Default to port 4000
+  // await app.listen(process.env.PORT ?? 4000); // Default to port 4000 for localy
+  await app.listen(process.env.PORT ?? 3000); // Default to port 4000 for render
+
 }
 
 bootstrap();
